1946fb69268658c3313c878e3ad48164882c3da0
Kamitu Munguti <58506873+kamitu-sm@users.noreply.github.com>
2020-05-16 15:19:55 +0300

    Update cron_s3_backups.sh

--- cron_s3_backups/cron_s3_backups.sh
+++ cron_s3_backups/cron_s3_backups.sh
@@ -18,10 +18,28 @@ echo "Current Time : $current_time"
 
 
 # Specify log file for the scripts
-FILE="/var/log/cron_s3_backups_output.txt"
+LOG_FILE="/var/log/cron_s3_backups_output.log"
 
 # Check if output file exists if not create it
-if [[ ! -f $FILES ]]
-	then
-		touch $FILE
+if [[ ! -f $LOG_FILE ]]; then
+	touch $LOG_FILE
 fi
+
+# Run the aws s3 backup command but first echo date of script runtime to log file
+echo $current_time >> $LOG_FILE
+
+# Assuming that you have a bucket name my-first-backup-bucket on Amazon s3 and your local database PGDATA is at /db/apps/pgdata
+# Assuming that your backups directory is /db/backups/
+# Create the tar files
+
+filename="/db/backups/${current_time}_db.bak.tar.gz"
+echo "Creating Tar file for database" >> $LOG_FILE
+tar -czvf "$filename" /db/apps/pgdata >> $LOG_FILE 2>&1
+
+# Copy to S3
+echo "Copying to S3" >> $LOG_FILE
+aws s3 cp "$filename" s3://my-first-backup-bucket/ >> $LOG_FILE 2>&1
+
+echo "Command Completed" >> $LOG_FILE
+echo "" >> $LOG_FILE
+echo "" >> $LOG_FILE
